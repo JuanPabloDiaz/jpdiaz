f8927e543efac4c2e474918fcd246aba5beb32b6
JuanPabloDiaz <juanchodis@hotmail.com>
2024-05-20 07:15:30 -0400

    chore: Remove unused import in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
 });
