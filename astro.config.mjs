bbaaee12be9cbf62ba34d6791f383175e0b2f093
JuanPabloDiaz <juanchodis@hotmail.com>
2025-03-29 12:56:07 -0400

    refactor: remove @astrojs/image dependency and update imports to astro:assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
@@ -6,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx(), image(), sitemap()],
-    site: 'https://jpdiaz.dev/',
+	integrations: [tailwind(), mdx(), sitemap()],
+	site: 'https://jpdiaz.dev/',
 });
