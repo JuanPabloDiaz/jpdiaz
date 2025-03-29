9f4cb89cd4d5318f34008281125749f61f943751
JuanPabloDiaz <juanchodis@hotmail.com>
2025-03-29 13:58:42 -0400

    refactor: update README for yarn usage and adjust icon imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), sitemap()],
+	integrations: [tailwind(), mdx(), sitemap(), icon()],
 	site: 'https://jpdiaz.dev/',
 });
