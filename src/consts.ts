0aaf8809a173b122f09bd150de15fc953f890431
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-05-13 09:25:58 -0400

    refactor(components): remove unused imports and centralize social links
    Centralize social links into a shared consts file and remove unused imports across components. This improves maintainability and reduces redundancy in the codebase.

--- src/consts.ts
+++ src/consts.ts
@@ -1,27 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-import type { AnalyticsConfig } from './types/analyticsTypes';
-
-/**
- * title {string} website title
- * favicon {string} website favicon url
- * description {string} website description
- * author {string} author
- * avatar {string} Avatar used in the profile
- * motto {string} used in the profile
- * url {string} Website link
- * baseUrl {string} When using GitHubPages, you must enter the repository name, startWith '/', e.g. /repo_name
- * recentBlogSize {number} Number of recent articles displayed in the sidebar
- * archivePageSize {number} Number of articles on archive pages
- * postPageSize {number} Number of articles on blog pages
- * feedPageSize {number} Number of articles on feed pages
- * beian {string} Chinese policy
- * asideTagsMaxSize {number}
- *    0: disable,
- *    > 0: display the limited number of tags in the sidebar
- *    All tags will be displayed in single page "/tags".
- */
 export const site = {
 	title: 'Juan Diaz', // required
 	favicon: '/favicon.svg', // required
@@ -61,49 +40,129 @@ export const homePageMenu = [
 
 export const config = {
 	lang: 'en', // en | es
-	codeFoldingStartLines: 16, // Need to re-run the project to take effect
-
-	// memos config
-	memosUrl: '', // https://xxxx.xxx.xx
-	memosUsername: '', // login name
-	memosPageSize: 10, // number
 };
 
-/**
- * Navigator
- */
-export const categories = [
+export const social = [
 	{
-		name: 'Blog',
-		iconClass: 'ri-draft-line',
-		href: '/blog/1',
+		href: 'https://github.com/juanpablodiaz',
+		ariaLabel: 'github',
+		iconName: 'simple-icons:github',
 	},
+	{
+		href: 'https://linkedin.com/in/1diazdev',
+		ariaLabel: 'linkedin',
+		iconName: 'simple-icons:linkedin',
+	},
+	{
+		href: 'https://twitter.com/JuanDiaz_427',
+		ariaLabel: 'twitter',
+		iconName: 'simple-icons:twitter',
+	},
+	// {
+	// 	href: 'https://platzi.com/p/1diazdev/',
+	// 	ariaLabel: 'platzi',
+	// 	iconName: 'simple-icons:platzi',
+	// },
+];
 
+export const resumeCertifications = [
 	{
-		name: 'Archive',
-		iconClass: 'ri-archive-line',
-		href: '/archive/1',
+		url: 'https://credentials.databricks.com/3428c69a-03d0-4598-8429-55786b9d8ddd#acc.G6QlY0p3',
+		title: 'Databricks MVP',
+		year: '2025',
 	},
 	{
-		name: 'Message',
-		iconClass: 'ri-chat-1-line',
-		href: '/message',
+		url: 'https://credentials.databricks.com/7c7d153e-2986-4fb1-a38e-5b14e9b71365#acc.U5feJdOb',
+		title: 'Generative AI Fundamentals',
+		year: '2025',
 	},
 	{
-		name: 'Search',
-		iconClass: 'ri-search-line',
-		href: '/search',
+		url: 'https://credentials.databricks.com/f11020d3-09ad-47a5-a9a4-06b4ef5f9980#acc.Poi85KdA',
+		title: 'Databricks Fundamentals',
+		year: '2025',
 	},
 	{
-		name: 'More',
-		iconClass: 'ri-more-fill',
-		href: 'javascript:void(0);',
-		children: [
-			{
-				name: 'About',
-				iconClass: 'ri-information-line',
-				href: '/about',
-			},
-		],
+		url: 'https://www.freecodecamp.org/certification/1diazdev/javascript-algorithms-and-data-structures',
+		title: 'Legacy JavaScript',
+		year: '2025',
+	},
+	{
+		url: 'https://www.freecodecamp.org/certification/1diazdev/javascript-algorithms-and-data-structures-v8',
+		title: 'JavaScript and Data Structures',
+		year: '2025',
+	},
+	{
+		url: 'https://www.freecodecamp.org/certification/1diazdev/scientific-computing-with-python-v7',
+		title: 'Scientific Computing with Python',
+		year: '2025',
+	},
+	{
+		url: 'https://www.freecodecamp.org/certification/1diazdev/legacy-front-end',
+		title: 'Legacy Front End',
+		year: '2024',
+	},
+	{
+		url: 'https://freecodecamp.org/certification/1diazdev/front-end-development-libraries',
+		title: 'Front End Development Libraries',
+		year: '2023',
+	},
+	{
+		url: 'https://freecodecamp.org/certification/1diazdev/responsive-web-design',
+		title: 'Responsive Web Design',
+		year: '2022',
+	},
+	{
+		url: 'https://platzi.com/p/1diazdev/ruta/8350-cloud-servidores/diploma/detalle/',
+		title: 'Server Management',
+		year: '2022',
+	},
+	{
+		url: 'https://platzi.com/p/1diazdev/ruta/8422-cloud-digital-leader/diploma/detalle/',
+		title: 'Cloud Digital Leader',
+		year: '2022',
+	},
+	{
+		url: './certification/IT-AplicationSoftwareFundamentals.jpg',
+		title: 'IT in Application Software Fundamentals',
+		year: '2020',
+		isWide: false,
+	},
+];
+
+export const resumeCourses = [
+	{
+		url: 'https://platzi.com/p/1diazdev/curso/7396-course/diploma/detalle/',
+		title: 'React course with Vite.js & Tailwind CSS',
+	},
+	{
+		url: 'https://platzi.com/p/1diazdev/curso/6867-sass/diploma/detalle/',
+		title: 'Learn Sass: Fundamentals!',
+	},
+	{
+		url: 'https://platzi.com/p/1diazdev/curso/6207-astro/diploma/detalle/',
+		title: 'Build a website with Astro',
+	},
+	{
+		url: 'https://platzi.com/p/1diazdev/curso/7758-web-chatgpt/diploma/detalle/',
+		title: 'Build a website with ChatGPT',
+	},
+	{
+		url: 'https://platzi.com/p/1diazdev/curso/3215-new-relic/diploma/detalle/',
+		title: 'Learn Observability: Fundamentals!',
+	},
+];
+
+export const resumePageMenu = [
+	{
+		title: 'Profile',
+		path: '#top',
+	},
+	{
+		title: 'Education',
+		path: '#education',
+	},
+	{
+		title: 'Experience',
+		path: '#experience',
 	},
 ];
