4b9013c52e1a116f69c98c76f39f2795aa30fb9f
Juan Diaz <juanchodis@hotmail.com>
2025-05-25 08:18:19 -0400

    Dev lint (#128)
    * refactor: reorganize dependencies in package.json for clarity
    
    * refactor: update package dependencies and add ESLint configuration
    
    * refactor: add local lighthouse command to package.json
    
    * Refactor code structure for improved readability and maintainability
    
    * refactor: add block scoping to switch case statements in defaultLabel function

--- src/plugins/remark-github-card.js
+++ src/plugins/remark-github-card.js
@@ -1,11 +1,11 @@
-import {h as _h, s as _s} from "hastscript";
-import {visit} from "unist-util-visit";
+import {h as _h, s as _s} from 'hastscript';
+import {visit} from 'unist-util-visit';
 
 /** Hacky function that generates an mdast HTML tree ready for conversion to HTML by rehype. */
 function h(el, attrs = {}, children = []) {
   const {tagName, properties} = _h(el, attrs);
   return {
-    type: "paragraph",
+    type: 'paragraph',
     data: {hName: tagName, hProperties: properties},
     children,
   };
@@ -15,13 +15,13 @@ function h(el, attrs = {}, children = []) {
 export function remarkGithubCard() {
   const transformer = (tree) => {
     visit(tree, (node, index, parent) => {
-      if (node.type !== "leafDirective") {
+      if (node.type !== 'leafDirective') {
         return;
       }
       if (!parent || index === undefined) {
         return;
       }
-      if (node.name !== "github") {
+      if (node.name !== 'github') {
         return;
       }
       /**
@@ -37,57 +37,57 @@ export function remarkGithubCard() {
        * }
        */
 
-      const repo = node.attributes.repo ? node.attributes.repo : ''
+      const repo = node.attributes.repo ? node.attributes.repo : '';
       if (!repo || !repo.includes('/')) {
         return h(
           'div',
           {class: 'hidden'},
           'Invalid repository. ("repo" attributte must be in the format "owner/repo")',
-        )
+        );
       }
-      const author = repo.split('/')[0]
-      const repoName = repo.split('/')[1]
+      const author = repo.split('/')[0];
+      const repoName = repo.split('/')[1];
 
-      const cardUuid = `GC${Math.random().toString(36).slice(-6)}` // Collisions are not important
+      const cardUuid = `GC${Math.random().toString(36).slice(-6)}`; // Collisions are not important
 
-      const nAvatar = h(`img#${cardUuid}-avatar`, {class: 'github-avatar mr-4',})
+      const nAvatar = h(`img#${cardUuid}-avatar`, {class: 'github-avatar mr-4',});
 
 
       const nTitle = h('div', {class: 'flex items-center justify-between'}, [
         h('a', {class: 'flex items-center text-inherit text-xl', href: `https://github.com/${repo}`, target: '_blank',}, [
           nAvatar,
-          h('div', {class: ''}, [{type: "text", value: author}]),
-          h('div', {class: 'mx-1'}, [{type: "text", value: '/'}]),
-          h('div', {class: 'font-bold break-all truncate',}, [{type: "text", value: repoName}]),
+          h('div', {class: ''}, [{type: 'text', value: author}]),
+          h('div', {class: 'mx-1'}, [{type: 'text', value: '/'}]),
+          h('div', {class: 'font-bold break-all truncate',}, [{type: 'text', value: repoName}]),
         ]),
-      ])
+      ]);
 
       const nDescription = h(
         `div#${cardUuid}-description`,
         {class: 'my-2'}, [
-          {type: "text", value: 'Waiting for api.github.com...',},
+          {type: 'text', value: 'Waiting for api.github.com...',},
         ]
-      )
+      );
 
       const nStars = h('div', {class: 'flex items-center'}, [
         h('i', {class: 'ri-star-line',}, []),
-        h(`div#${cardUuid}-stars`, {class: 'ml-1 mr-4'}, [{type: "text", value: "Waiting"}])
-      ])
+        h(`div#${cardUuid}-stars`, {class: 'ml-1 mr-4'}, [{type: 'text', value: 'Waiting'}])
+      ]);
       const nForks = h('div', {class: 'flex items-center'}, [
         h('i', {class: 'ri-git-fork-line',}, []),
-        h(`div#${cardUuid}-forks`, {class: 'ml-1 mr-4'}, [{type: "text", value: "Waiting"}])
-      ])
+        h(`div#${cardUuid}-forks`, {class: 'ml-1 mr-4'}, [{type: 'text', value: 'Waiting'}])
+      ]);
 
       const nLicense = h('div', {class: 'flex items-center'}, [
         h('i', {class: 'ri-copyright-line',}, []),
-        h(`div#${cardUuid}-license`, {class: 'ml-1 mr-4'}, [{type: "text", value: "Waiting"}])
-      ])
+        h(`div#${cardUuid}-license`, {class: 'ml-1 mr-4'}, [{type: 'text', value: 'Waiting'}])
+      ]);
       const nScript = h(
         `script#${cardUuid}-script`,
         {type: 'text/javascript', defer: true},
         [
           {
-            type: "script", value: `
+            type: 'script', value: `
       fetch('https://api.github.com/repos/${repo}', { referrerPolicy: "no-referrer" }).then(response => response.json()).then(data => {
         if (data.description) {
         document.getElementById('${cardUuid}-description').innerText = data.description.replace(/:[a-zA-Z0-9_]+:/g, '');
@@ -111,7 +111,7 @@ export function remarkGithubCard() {
         console.warn("[GITHUB-CARD] (Error) Loading card for ${repo} | ${cardUuid}.")
       }) `,
           }]
-      )
+      );
 
       parent.children[index] = h(
         `div#${cardUuid}-card`,
@@ -127,7 +127,7 @@ export function remarkGithubCard() {
           h('div', {class: 'flex'}, [nStars, nForks, nLicense]),
           nScript
         ],
-      )
+      );
     });
   };
 
