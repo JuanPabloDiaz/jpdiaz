84282a3b3c06a1aa97eac9049814340d6e33032b
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-06-05 08:17:20 -0400

    fix: update type definition to allow undefined for blog and rss types in BlogSearch and config

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,44 +9,46 @@ export const config = {
 };
 
 const projectCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
-  schema: ({ image }) => z.object({
-    inProgress: z.boolean(),
-    title: z.string(),
-    description: z.string(),
-    img_alt: z.string().optional(),
-    link: z.string().optional().nullable(),
-    github_link: z.string().optional(),
-    tags: z.array(z.string()),
-    image: image().optional(),
-  }),
+	type: 'content', // v2.5.0 and later
+	schema: ({ image }) =>
+		z.object({
+			inProgress: z.boolean(),
+			title: z.string(),
+			description: z.string(),
+			img_alt: z.string().optional(),
+			link: z.string().optional().nullable(),
+			github_link: z.string().optional(),
+			tags: z.array(z.string()),
+			image: image().optional(),
+		}),
 });
 
 const blogCollection = defineCollection({
-  type: 'content',
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: image().optional(),
-    tags: z.array(z.string()),
-    lang: z.enum(i18nLangs),
-    draft: z.boolean().optional(),
-    author: z.string().optional(),
-    // SEO related fields, if you want them per-post
-    metaTitle: z.string().optional(),
-    metaDescription: z.string().optional(),
-    ogImage: image().optional(), // For specific OG image per post
-    pageKeywords: z.array(z.string()).optional(),
-  }),
+	type: 'content',
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+			tags: z.array(z.string()),
+			lang: z.enum(i18nLangs),
+			draft: z.boolean().optional(),
+			author: z.string().optional(),
+			type: z.enum(['blog', 'rss']).optional().default('blog'),
+			metaTitle: z.string().optional(),
+			metaDescription: z.string().optional(),
+			ogImage: image().optional(), // For specific OG image per post
+			pageKeywords: z.array(z.string()).optional(),
+		}),
 });
 
 export const collections = {
-  projects: projectCollection,
-  blog: blogCollection,
-  'more-full-stack-projects': projectCollection,
-  'more-open-source': projectCollection,
-  'more-projects': projectCollection,
-  'open-source': projectCollection,
+	projects: projectCollection,
+	blog: blogCollection,
+	'more-full-stack-projects': projectCollection,
+	'more-open-source': projectCollection,
+	'more-projects': projectCollection,
+	'open-source': projectCollection,
 };
